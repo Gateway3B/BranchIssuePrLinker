33bfd5e85ac58b6493b1a61f235169007a50a714
Matthew Weisfeld <matthewedwardweisfeld@gmail.com>
2022-01-01 11:51:23 -0500

    Init

---
+++ README.md
@@ -0,0 +1,44 @@
+# Branch Issue Pr Linker
+
+This action has several functions to keep branches, issues, and prs linked together.
+
+## On Push
+
+On a push this action will validate the branch name against this format:
+
+`{feature/bug}/{issue number}/This-Is-A-Feature-Or-A-Bug`
+
+It will search for open issues on your repository for the issue with the corresponding issue number.
+
+### On Successful Validation
+
+On seccessful validation of both branch name and issue number, the action will search for existing pull requests with this branch.
+
+If a pull request does not already exist, it will create a pull request from your branch to the assumed `develop` branch with the passed in message. We recommend passing in the commit message.
+
+## On Pull Request
+
+On a pull request this action will validate the branch name against this format:
+
+`{feature/bug}/{issue number}/This-Is-A-Feature-Or-A-Bug`
+
+It will also assert only one commit in the pull request.
+
+## Inputs
+
+### `github-token`
+
+**Required** The github token provided by the action.
+
+### `pr-commit-message`
+
+**Requred** The commit message to be used as the message for any created prs.
+
+## Example Usage
+
+```
+uses: Gateway3B/BranchIssuePrLinker@master
+with:
+    github-token: ${{ github.token }}
+    pr-commit-message: ${{ github.event.commits[0].message }}
+```
---
+++ index.js
@@ -0,0 +1,91 @@
+const { getInput, setFailed } = require('@actions/core');
+const { getOctokit, context } = require('@actions/github');
+
+const regex = "/(feature|bug){1}\/[0-9]+\/[A-Z]{1}([a-z]|[A-Z]|[0-9]|-[A-Z]{1})*/g)";
+const token = getInput('github-token', { require: true });
+const octokit = getOctokit(token);
+const branchName = context.ref.replace('refs/heads/', '');
+let issueNumber;
+
+async function action() {
+    validateBranchName(branchName);
+    
+    issueNumber = validateIssue(branchName);
+
+    switch(context.eventName) {
+        case 'push':
+            push();
+            break;
+
+        case 'pull_request':
+            pullRequest();
+            break;
+
+        default:
+            setFailed('Invalid Event');
+    }
+}
+
+function push() {
+    const results = await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
+        owner: context.repo.owner, 
+        repo: context.repo.name,
+        commit_sha: context.sha
+    });
+
+    if(results.data.filter(pr => pr.state === 'open').length() === 0) {
+        getOctokit(token).rest.pulls.create({
+            owner: context.repo.owner, 
+            repo: context.repo.name,
+            title: getInput('pr-commit-message', { require: true }),
+            head: branchName,
+            base: 'develop',
+            issue: issueNumber
+        });
+    }
+}
+
+function pullRequest() {
+    const pull = await octokit.rest.pulls.get({
+        owner: context.repo.owner, 
+        repo: context.repo.name,
+        pull_number: context.payload.pull_request.number
+    }).catch(err => {
+        core.setFailed('Pull request does not exist. Please create pull request.');
+        throw new Error(err);
+    });
+
+    if(pull.data.commits > 1) {
+        setFailed('PRs can only have one commit. Please sqaush your commits down.')
+        throw new Error();
+    }
+}
+
+function validateBranchName(branchName) {
+    if(!branchName.match(regex)) {
+        setFailed('Branch name does not match. ex: feature/132/This-Is-A-Feature');
+        throw new Error();
+    }
+}
+
+async function validateIssue(branchName) {
+    const issueNumber = branchName.split('/')[1];
+    await getOctokit().rest.issues.get(
+        { 
+            owner: github.context.payload.repository.owner, 
+            repo: github.context.payload.repository.name,
+            issue_number: issueNumber
+        }
+    ).catch(err => {
+        setFailed('Branch issue number does not exist. ex: feature/132/This-Is-A-Feature; Issue #132 does not exist.');
+        throw err;
+    });
+
+    return issueNumber;
+}
+
+try {
+    action();
+} catch {
+    setFailed('Internal Action Error');
+}
