7ef879324b8a2a0744eccda69e312d5fc04ce513
Matthew Weisfeld <matthewedwardweisfeld@gmail.com>
2022-01-02 20:08:02 -0500

    Regix fix.

--- test.js
+++ test.js
@@ -2,25 +2,28 @@
 const { getInput, setFailed } = require('@actions/core');
 const { getOctokit, context } = require('@actions/github');
 
-const regex = "/(feature|bug){1}\/[0-9]+\/[A-Z]{1}([a-z]|[A-Z]|[0-9]|-[A-Z]{1})*/g)";
+const regex = /(feature|bug){1}\/[0-9]+\/[A-Z]{1}([a-z]|[A-Z]|[0-9]|-[A-Z]{1})*/g
 const testToken = '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';
 const octokit = getOctokit(testToken);
 
 
 async function test() {
-    const pulls = await octokit.rest.pulls.get(
-        { 
-            owner: 'Gateway3B', 
-            repo: 'donation-engine-backend',
-            pull_number: 2
-        }
-    ).catch(err => {
-        core.setFailed('Branch issue number does not exist. ex: feature/132/This-Is-A-Feature; Issue #132 does not exist.');
-    });
+    // const pulls = await octokit.rest.pulls.get(
+    //     { 
+    //         owner: 'Gateway3B', 
+    //         repo: 'donation-engine-backend',
+    //         pull_number: 2
+    //     }
+    // ).catch(err => {
+    //     setFailed('Branch issue number does not exist. ex: feature/132/This-Is-A-Feature; Issue #132 does not exist.');
+    // });
     
-    
+    const test = "feature/1/This-Is-A-Test";
 
-    console.log(pulls);
+    if(test.match(regex))
+        console.log('hi');
+    console.log('bye');
+    // console.log(pulls);
 }
 
 test();
