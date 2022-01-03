24b7d1027e1aede1e9ebea33a2606c8eb343267f
Matthew Weisfeld <matthewedwardweisfeld@gmail.com>
2022-01-02 19:52:01 -0500

    Test Issue (#1)
    Info messages.
    
    Log message.
    
    Ncc recompile.
    
    Removed open regex )
    
    ncc recompile
    
    Cleanup
    
    Regix fix.
    
    Trigger change.
    
    Added more info messages.
    
    Octokit reference fix.
    
    Added ncc compile.
    
    Github reference fix.
    
    Remove test.js. Index number out.
    
    Repo reference fixes. Error throwing cleanup.
    
    Await fix. Debugging added.
    
    Typo
    
    Repo reference issue fix.
    
    Length refernce fix.
    
    Force issue number to int.
    
    Removed message input.
    
    Forgot to compile.
    
    Pull Request branch name get fix.
    
    Check for existing pr fix.
    
    Changed squash message to warning.

--- index.js
+++ index.js
@@ -1,91 +1,111 @@
-const { getInput, setFailed } = require('@actions/core');
+const { getInput, setFailed, info, warning } = require('@actions/core');
 const { getOctokit, context } = require('@actions/github');
 
-const regex = "/(feature|bug){1}\/[0-9]+\/[A-Z]{1}([a-z]|[A-Z]|[0-9]|-[A-Z]{1})*/g)";
+const regex = /(feature|bug){1}\/[0-9]+\/[A-Z]{1}([a-z]|[A-Z]|[0-9]|-[A-Z]{1})*/g;
 const token = getInput('github-token', { require: true });
 const octokit = getOctokit(token);
-const branchName = context.ref.replace('refs/heads/', '');
+let branchName;
 let issueNumber;
 
 async function action() {
-    validateBranchName(branchName);
+    try {    
+        switch(context.eventName) {
+            case 'push':
+                info('Processing Push');
+                await push();
+                break;
+                
+            case 'pull_request':
+                info('Processing Pull Request');
+                await pullRequest();
+                break;
     
-    issueNumber = validateIssue(branchName);
-
-    switch(context.eventName) {
-        case 'push':
-            await push();
-            break;
-
-        case 'pull_request':
-            await pullRequest();
-            break;
-
-        default:
-            setFailed('Invalid Event');
-    }
+            default:
+                setFailed('Invalid Event');
+        }
+    } catch(err) {}
 }
 
 async function push() {
-    const results = await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
-        owner: context.repo.owner, 
-        repo: context.repo.name,
-        commit_sha: context.sha
-    });
+    this.branchName = context.ref.replace('refs/heads/', '');
 
-    if(results.data.filter(pr => pr.state === 'open').length() === 0) {
-        getOctokit(token).rest.pulls.create({
-            owner: context.repo.owner, 
-            repo: context.repo.name,
-            title: getInput('pr-commit-message', { require: true }),
+    validateBranchName();
+        
+    await validateIssue();
+
+    const results = await octokit.rest.pulls.list({
+        owner: context.repo.owner, 
+        repo: context.repo.repo,
+        state: 'open',
+        head: this.branchName,
+        base: 'develop'
+    })
+
+    if(results.data.length === 0) {
+        await octokit.rest.pulls.create({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
             head: branchName,
             base: 'develop',
             issue: issueNumber
+        }).catch(err => {
+            setFailed('Error Creating Pull Request');
+            throw err;
         });
+
+        info('New Pull Request Created');
+    } else {
+        info('Pull Request Already Open');
     }
 }
 
 async function pullRequest() {
     const pull = await octokit.rest.pulls.get({
         owner: context.repo.owner, 
-        repo: context.repo.name,
+        repo: context.repo.repo,
         pull_number: context.payload.pull_request.number
     }).catch(err => {
-        core.setFailed('Pull request does not exist. Please create pull request.');
-        throw new Error(err);
-    });
-
-    if(pull.data.commits > 1) {
-        setFailed('PRs can only have one commit. Please sqaush your commits down.')
-        throw new Error();
-    }
-}
-
-function validateBranchName(branchName) {
-    if(!branchName.match(regex)) {
-        setFailed('Branch name does not match. ex: feature/132/This-Is-A-Feature');
-        throw new Error();
-    }
-}
-
-async function validateIssue(branchName) {
-    const issueNumber = branchName.split('/')[1];
-    await getOctokit().rest.issues.get(
-        { 
-            owner: github.context.payload.repository.owner, 
-            repo: github.context.payload.repository.name,
-            issue_number: issueNumber
-        }
-    ).catch(err => {
-        setFailed('Branch issue number does not exist. ex: feature/132/This-Is-A-Feature; Issue #132 does not exist.');
+        setFailed('Pull request does not exist. Please create pull request.');
         throw err;
     });
 
-    return issueNumber;
+    this.branchName = pull.data.head.ref;
+
+    validateBranchName();
+        
+    await validateIssue();
+
+    if(pull.data.commits > 1) {
+        warning('PRs should only have one commit. Please sqaush your commits down.')
+    }
+
+    info('Pull Request Validated');
 }
 
-try {
-    action();
-} catch {
-    setFailed('Internal Action Error');
+function validateBranchName() {
+    if(!this.branchName.match(regex)) {
+        setFailed('Branch name does not match. ex: feature/132/This-Is-A-Feature');
+        throw new Error();
+    }
+    info('Branch Name Validated')
 }
+
+async function validateIssue() {
+    const issueNumber = parseInt(this.branchName.split('/')[1], 10);
+    await octokit.rest.issues.get(
+        { 
+            owner: context.repo.owner, 
+            repo: context.repo.repo,
+            issue_number: issueNumber
+        }
+    ).catch(err => {
+        setFailed(`Branch issue number does not exist #${issueNumber}. ex: feature/132/This-Is-A-Feature; Issue #132 does not exist.`);
+        throw err;
+    });
+
+    info('Issue Number Validated')
+
+    this.issueNumber = issueNumber;
+}
+
+action();
